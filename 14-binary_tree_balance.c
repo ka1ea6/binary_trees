da32a74ab0c94e832e8ec0a75cf814803ad8c742
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-14 09:16:41 +0300

    14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - function to measure the balance factor of
+ * a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the balance
+ * factor.
+ *
+ * Return: balance factor of a binary tree or 0 if tree is NULL.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance = 0;
+	const binary_tree_t *temp;
+
+	if (!tree)
+		return (balance);
+	
+	temp = malloc(sizeof(binary_tree_t));
+
+	if (!temp)
+		return (balance);
+
+	temp = tree;
+
+	while (temp->left)
+	{
+		balance++;
+		temp = temp->left;
+	}
+	temp = tree;
+	while (temp->right)
+	{
+		balance--;
+		temp = temp->right;
+	}
+
+	return (balance);
+}
