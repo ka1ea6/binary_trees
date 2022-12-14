cf3aaccdc9808a9590e95e6ffeb9cc580aa13b4d
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-14 11:31:52 +0300

    14

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,13 +11,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left= 0, height_right = 0;
+	size_t height_left = 0, height_right = 0;
 
 	if (!tree)
-		return (height);
+		return (0);
 
-	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-	
-	return ((height_right > height_right) ? height_right : height_left);
+	if (tree->left || tree->right)
+	{
+		height_left = binary_tree_height(tree->left) + 1;
+		height_right = binary_tree_height(tree->right) + 1;
+	}
+
+	return ((height_left >= height_right) ? height_left : height_right);
 }
